ea092eb16ac1eb1ad2908f69aaa6925f01322c0d
Ui-Seok <kasasima18@gmail.com>
2022-08-18 14:45:10 +0900

    Edit code

--- read_npy.py
+++ read_npy.py
@@ -1,8 +1,28 @@
 import numpy as np
 import glob
+import shutil
+import os
 
-for i in glob.glob('./result/Burglary/*.npy'):
+for i in glob.glob('./result/Robbery/*.npy'):
     check = np.load(i)
+    print('============================')
+    print(i)
+    print(max(check[:]))
     if check.shape[0] != 32:
         print(i)
         print((check.shape))
+
+
+# # Video move code
+# result_path = './result/Robbery/'
+# video_path = './dataset/UCF_Crime/Robbery/'
+
+# get_files = os.listdir(result_path)
+# video_files = os.listdir(video_path)
+# file_list = []
+# for i in range(len(get_files)):
+#     file_list.append(get_files[i][:15] + '.mp4')
+
+# for i in video_files:
+#     if i in file_list:
+#         shutil.move(video_path + i, './dataset/UCF_Crime')
